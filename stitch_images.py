d85fc88940efba042a9d5c5f1c12b7cd86fb5d8f
Gunnar Hellmund Laier <info@addastat.dk>
2024-08-16 19:29:21 +0200

    Update stitch_images.py

--- stitch_images.py
+++ stitch_images.py
@@ -33,19 +33,41 @@ def compute_overlap(img1, img2):
 
     return best_overlap
 
-def adjust_images(img1, img2, overlap):
-    """Adjust the images by cropping based on the overlap value."""
-    width, height1 = img1.size
-    _, height2 = img2.size
+def adjust_header(img2, max_overlap):
+    """Remove the header from the second image based on the maximum overlap."""
+    img2_gray = image_to_grayscale(img2)
+    width, height = img2_gray.size
+    best_overlap = max_overlap
+    previous_overlap = -1
 
-    if overlap > 0:
-        img1 = img1.crop((0, 0, width, height1 - overlap))
-        img2 = img2.crop((0, overlap, width, height2))
-    elif overlap < 0:
-        img1 = img1.crop((0, -overlap, width, height1))
-        img2 = img2.crop((0, 0, width, height2 + overlap))
+    # Incrementally remove rows from the top of img2 until overlap decreases
+    for top in range(height):
+        img2_part = np.array(img2_gray.crop((0, top, width, height)))
+        if top > 0:
+            previous_overlap = compute_overlap(img1, Image.fromarray(img2_part))
+        if previous_overlap < max_overlap:
+            img2 = img2.crop((0, top, width, height))
+            break
 
-    return img1, img2
+    return img2
+
+def adjust_footer(img1, max_overlap):
+    """Remove the footer from the first image based on the maximum overlap."""
+    img1_gray = image_to_grayscale(img1)
+    width, height = img1_gray.size
+    best_overlap = max_overlap
+    previous_overlap = -1
+
+    # Incrementally remove rows from the bottom of img1 until overlap decreases
+    for bottom in range(height):
+        img1_part = np.array(img1_gray.crop((0, 0, width, height - bottom)))
+        if bottom > 0:
+            previous_overlap = compute_overlap(Image.fromarray(img1_part), img2)
+        if previous_overlap < max_overlap:
+            img1 = img1.crop((0, 0, width, height - bottom))
+            break
+
+    return img1
 
 def create_long_screenshot(folder_path, output_path):
     """Create a long screenshot by stitching together images."""
@@ -63,6 +85,10 @@ def create_long_screenshot(folder_path, output_path):
     for i in range(1, len(images)):
         overlap = compute_overlap(adjusted_images[-1], images[i])
         img1, img2 = adjust_images(adjusted_images[-1], images[i], overlap)
+
+        img2 = adjust_header(img2, overlap)
+        img1 = adjust_footer(img1, overlap)
+
         adjusted_images[-1] = img1
         adjusted_images.append(img2)
 
